3c394fb78ec5080e5bccb6f3ac156d72704a6e9f
jasonwalsh <rightlag@gmail.com>
2019-07-24 16:16:34 -0400

    fix: add perfstandbyok query param to health check

--- main.tf
+++ main.tf
@@ -289,7 +289,7 @@ module "alb" {
     health_check_healthy_threshold   = 3
     health_check_interval            = 10
     health_check_matcher             = "200-299"
-    health_check_path                = "/v1/sys/health?standbyok=true"
+    health_check_path                = "/v1/sys/health?standbyok=true&perfstandbyok=true"
     health_check_port                = "traffic-port"
     health_check_timeout             = 5
     health_check_unhealthy_threshold = 3
